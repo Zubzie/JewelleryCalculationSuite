9d84b2694f67ff090c3ccae208368b558b28f262
zubzie <jake_zubov@outlook.com.au>
2022-09-30 17:43:59 +1000

    started project, made metal converter, added ring sizes

---
+++ Metal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JewelleryProgramV3
+{
+    internal class Metal
+    {
+        public string Name { get; set; }
+        public double SpecificGravity { get; set; }
+
+        public Metal(string name, double specificGravity)
+        {
+            this.Name = name;
+            this.SpecificGravity = specificGravity;
+        }
+    }
+}
